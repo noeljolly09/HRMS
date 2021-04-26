429d55cc2d4823a5310396d460edf517c87ce56f
noeljolly09 <62985022+noeljolly09@users.noreply.github.com>
2021-04-26 21:36:38 +0530

    Update settings.py

--- HRMS/HRMS/settings.py
+++ HRMS/HRMS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-unzg^)$4kdgzxm16820f#e%%)#z8+sn6@fi#(j!lnvht1-m8$4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
